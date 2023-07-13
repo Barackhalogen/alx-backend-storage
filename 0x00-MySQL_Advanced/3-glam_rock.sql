baa50b750df15c57160d605b1acd38788fa35884
Barackhalogen <barackhalogen@gmail.com>
2023-07-13 14:18:25 +0000

    Fixed

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,5 @@
--- Lists all bands with Glam rock as their style,
--- ranked by their longevity
--- Column names must be: band_name & lifespan
-
+-- This SQL script select band_name, and lifespan column which is difference
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
-FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
-ORDER BY lifespan DESC;
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
